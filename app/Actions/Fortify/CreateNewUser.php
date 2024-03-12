7910acf18ae3a11ca43161fbc6c9d624fdb6c388
Rean Joy Cicat <reanjoycatci@gmail.com>
2024-03-12 14:51:20 +0800

    First commit

---
+++ app/Actions/Fortify/CreateNewUser.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Actions\Fortify;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Fortify\Contracts\CreatesNewUsers;
+use Laravel\Jetstream\Jetstream;
+
+class CreateNewUser implements CreatesNewUsers
+{
+    use PasswordValidationRules;
+
+    /**
+     * Validate and create a newly registered user.
+     *
+     * @param  array<string, string>  $input
+     */
+    public function create(array $input): User
+    {
+        Validator::make($input, [
+            'firstname' => ['required', 'string', 'max:255'],
+            'lastname' => ['required', 'string', 'max:255'],
+            'barangay' => ['required', 'string', 'max:255'],
+            'city' => ['required', 'string', 'max:255'],
+            'phone' => ['required', 'digits:11'], // Adjust '10' to the actual number of digits allowed
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],        
+            'password' => $this->passwordRules(),
+            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
+        ])->validate();
+
+        return User::create([
+            'firstname' => $input['firstname'],
+            'lastname' => $input['lastname'],
+            'phone' => $input['phone'],
+            'email' => $input['email'],
+            'barangay' => $input['barangay'],
+            'city' => $input['city'],
+            'password' => Hash::make($input['password']),
+        ]);
+    }
+}
